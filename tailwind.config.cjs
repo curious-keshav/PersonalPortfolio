c0a243d5ffd22e5c0b5b98ac8f0c3b5872722366
Keshav Verma <keshavverma532@gmail.com>
2023-08-21 00:51:39 +0530

    animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,8 @@ module.exports = {
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
+        transitionFirst: "#33393B",
+        transitionSecond: "#606567",
       },
       animation: {
         'spin-slow': 'spin 8s linear infinite',
